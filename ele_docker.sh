9a0edd99bb97e9755538469105bd6f8734c8d6cb
multimeters <49971761+multimeters@users.noreply.github.com>
2024-01-17 15:27:25 +0800

    Update ele_docker.sh

--- ele_docker.sh
+++ ele_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is a simple example script
 
-docker run --rm -it --runtime=nvidia --gpus all -e DISPLAY  -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix  -v $HOME/.Xauthority:/root/.Xauthority  multimeters/elevation-mapping-cupy:11.6.2-base-ubuntu20.04-noetic-cudnn8.2.2.26-elemapping-ssh-pytorch
+docker run -it --runtime=nvidia --gpus all -e DISPLAY  -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix  -v $HOME/.Xauthority:/root/.Xauthority  multimeters/elevation-mapping-cupy:11.6.2-base-ubuntu20.04-noetic-cudnn8.2.2.26-elemapping-ssh-pytorch
